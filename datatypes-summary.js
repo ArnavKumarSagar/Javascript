8bdb5dc3ab1c917d38e86f61b1f3858ebf301321
ArnavKumarSagar <arnav.kr.sagar@gmail.com>
2025-03-19 20:00:03 +0000

    summary

---
+++ datatypes-summary.js
@@ -0,0 +1,12 @@
+// primitive datatypes
+    // 7 types: strings,number,boolean,null,undefined,symbol,BigInt
+
+// reference(non primitive)
+    // Array,Object,Functions
+
+    const id=Symbol("123")
+    const ANotherid=Symbol("123")
+    console.log(id);
+    console.log(ANotherid);
+    console.log(id==ANotherid)
+    
