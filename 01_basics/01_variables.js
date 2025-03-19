c0f3be5c649745637409aaec17c0d55367f269be
ArnavKumarSagar <arnav.kr.sagar@gmail.com>
2025-03-19 17:30:14 +0000

    done with basic of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=1445
+let accounteamail="123@gmail.com"
+var accountPassword="123442"
+// prefer not to use var for declaring variable ,
+// because of block scope and functional scope
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// number
+let age=20
+
+// bigint
+// string => ""
+// boolean => true,false
+// null => standalone value
+// undefined
+// symbol =>
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+    
