236f775f351a63b692f69315b7819980ffa0072d
ArnavKumarSagar <arnav.kr.sagar@gmail.com>
2025-05-27 05:20:05 +0000

    arrow

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,10 @@
+// function print(){
+//     console.log("hello");
+    
+// }
+// print()
+
+function add2no(no1,no2){
+    console.log(no1+no2);
+}
+add2no("v","e")
---
+++ 03_basics/03_arrowfunc.js
@@ -0,0 +1,12 @@
+
+// const chai=function(){
+//     let username="arnav"
+    // console.log(this)        //output =>Object [global] {
+// }
+
+const chai=()=>{
+    let username="arnav"
+    console.log(this)        //output=>{}
+}
+
+chai()
