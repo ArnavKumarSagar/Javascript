ea64cedd660f9b6b3350bfbcd86522aad4d95d73
ArnavKumarSagar <arnav.kr.sagar@gmail.com>
2025-06-24 19:00:36 +0000

    filter

---
+++ 04_loops/four.js
@@ -0,0 +1,11 @@
+// const fruits=["apple","banana","grapes","mango"]
+// const values=fruits.forEach((item)=>{
+//     // console.log(item);
+//     return item ;
+// })           //for each loop returns undefined 
+// console.log(values);
+
+// filter method -it returns an array 
+const arr=[1,2,3,4,5,6,7,8,9,9]
+const nums=arr.filter((num) => {return num>4})
+console.log(nums);
---
+++ 04_loops/three.js
@@ -0,0 +1,16 @@
+// for of loop- it iterates over length and returns the value at every index 
+const fruits=["apple","banana","grapes","mango"]
+// for(let fruit of fruits){
+//     console.log(fruit)       //apple banana grapes mango
+// }
+
+
+// for in loop-it iterates over length and gives the index 
+// for(let index in fruits){
+//     console.log(index)          //0 1 2 3 
+// }
+
+// we can also print the value at that index by this method 
+// for(let index in fruits){
+//     console.log(fruits[index])
+// }
